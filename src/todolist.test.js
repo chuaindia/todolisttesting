4b100167302e1bc353c29195c9b1c820a2fec758
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-10-14 23:35:51 +0530

    Add updated js file for linterfree

--- src/todolist.test.js
+++ src/todolist.test.js
@@ -4,12 +4,12 @@
 import TodoList from './modules/todolist.js';
 
 describe('Test if function are working', () => {
-  const testToDoTask = new TodoList();   
+  const testToDoTask = new TodoList();
   test('Test addTask function is working', () => {
     testToDoTask.addTask('test1');
     testToDoTask.addTask('test2');
     expect(testToDoTask.listArray[0].description).toBe('test1');
-  });  
+  });
   test('Test delete function is working', () => {
     testToDoTask.removeTask(1);
     expect(testToDoTask.listArray.length).toBe(1);
