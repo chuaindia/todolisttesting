62c86bd6e5bde8e8b1f62ee4651e5ddf475dc423
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-10-15 03:39:58 +0530

    Add files for rectification following reviewer

--- src/todolist.test.js
+++ src/todolist.test.js
@@ -1,38 +1,35 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable indent */
-/* eslint-disable linebreak-style */
 /**
  * @jest-environment jsdom
  */
- import TodoList from './modules/todolist.js';
+import TodoList from './modules/todolist.js';
 
- describe('Test if functions are working', () => {
-   const testToDoTask = new TodoList();
- 
-   test('Test addTask function is working', () => {
-     testToDoTask.addTask('test1');
-     testToDoTask.addTask('test2');
-     expect(testToDoTask.listArray[0].description).toBe('test1');
-   });
- 
-   test('Test delete function is working', () => {
-     testToDoTask.removeTask(1);
-     expect(testToDoTask.listArray.length).toBe(1);
-   });
- 
-   test('Test if the changeComplete function is working', () => {
-     testToDoTask.changeComplete(1);
-     expect(testToDoTask.listArray[0].completed).toBe(true);
-   });
- 
-   test('Test if Edit function is working', () => {
-     testToDoTask.editTask(1, 'test1');
-     expect(testToDoTask.listArray[0].description).toBe('test1');
-   });
- 
-   test('Test  for clear Completed function is working properly', () => {
-     testToDoTask.clearCompleted();
-     expect(testToDoTask.listArray.length).toBe(0);
-   });
- });
+describe('Test i functions are working', () => {
+  const testToDoTask = new TodoList();
+
+  test('Test addTask function is working', () => {
+    testToDoTask.addTask('test1');
+    testToDoTask.addTask('test2');
+    expect(testToDoTask.listArray[0].description).toBe('test1');
+  });
+
+  test('Test delete function is working', () => {
+    testToDoTask.removeTask(1);
+    expect(testToDoTask.listArray.length).toBe(1);
+  });
+
+  test('Test if the changeComplete function is working', () => {
+    testToDoTask.changeComplete(1);
+    expect(testToDoTask.listArray[0].completed).toBe(true);
+  });
+
+  test('Test if Edit function is working', () => {
+    testToDoTask.editTask(1, 'test1');
+    expect(testToDoTask.listArray[0].description).toBe('test1');
+  });
+
+  test('Test for clear Completed function is working properly', () => {
+    testToDoTask.clearCompleted();
+    expect(testToDoTask.listArray.length).toBe(0);
+  });
+});
