7a391315f45b95c5adff759d2488e311e9b0b3c1
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-10-15 00:43:49 +0530

    Add updated js file for linterfree

--- src/modules/todolist.js
+++ src/modules/todolist.js
@@ -1,87 +1,59 @@
 export default class ToDoList {
-  constructor() {
-    this.getTasks();
-  }
-  
-  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
-  
-  updateTasks() {
-    localStorage.setItem('toDoList', JSON.stringify(this.listArray));
-  }
-  
-  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
-  
-  getTasks() {
-    this.listArray = JSON.parse(localStorage.getItem('toDoList')) || [];
-  }
-  
-  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
-  
-  showTasks = () => JSON.parse(localStorage.getItem('toDoList')) || [];
-  
-  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
-  
-  setEdit(i) {
-    const task = this.listArray.find(
-      (item) => parseInt(item.index, 10) === parseInt(i, 10),
-    );
-    task.edit = true;
-    this.updateTasks();
-  }
-  
-  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
-  
-  addTask(description) {
-    const task = {
-      description,
-      completed: false,
-      index: this.listArray.length + 1,
-      edit: false,
-    };
-    this.listArray = [...this.listArray, task];
-    this.updateTasks();
-  }
-  
-  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
-  
-  clearCompleted() {
-    this.listArray = this.listArray.filter((item) => item.completed !== true);
-    if (this.listArray.length > 0) {
-      this.listArray = this.listArray.map((list, i) => {
-        list.index = i + 1;
-        return list;
-      });
+    constructor() {
+        this.getTasks();
     }
-    this.updateTasks();
-  }
-  
-  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
-  
-  removeTask(index) {
-    this.listArray = this.listArray.filter((item) => item.index !== index);
-    this.listArray = this.listArray.map((list, i) => {
-      list.index = i + 1;
-      return list;
-    });
-    this.updateTasks();
-  }
-  
-  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
-  
-  editTask(index, description) {
-    this.listArray[index - 1].description = description;
-    this.listArray[index - 1].edit = false;
-    this.updateTasks();
-  }
-  
-  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
-  
-  changeComplete(i) {
-    const status = this.listArray[i - 1].completed;
-    this.listArray[i - 1] = {
-      ...this.listArray[i - 1],
-      completed: !status,
-    };
-    this.updateTasks();
-  }
+    updateTasks() {
+        localStorage.setItem('toDolist', JSON.stringify(this.listArray));
+    }
+    getTasks() {
+        this.listArray = JSON.parse(localStorage.getItem('toDoList')) || [];
+    }
+    showTasks = () => JSON.parse(localStorage.getItem('toDoList')) || [];
+    setEdit(i) {
+        const task = this.listArray.find(
+            (item) => parseInt(item.index, 10) === parseInt(i, 10),
+        );
+        task.edit = true;
+        this.updateTasks();
+    }
+    addTask(description) {
+        const ask = {
+            description,
+            completed: false,
+            index: this.listArray.length + 1,
+            edit: false,
+        };
+        this.listArray = [...this.listArray, task];
+        this.updateTasks();
+    }
+    clearCompleted() {
+        this.listArray = this.listArray.filter((item) => item.completed !== true);
+        if (this.listArray.length > 0) {
+            this.listArray = this.listArray.map((list, i) => {
+                list.index = i + 1;
+                return list;
+            });
+        }
+        this.updateTasks();
+    }
+    removeTask(index) {
+        this.listArray = this.listArray.filter((item) => item.index !== index);
+        this.listArray = this.listArray.map((list, i) => {
+            list.index = i + 1;
+            return list;
+        });
+        this.updateTasks();
+    }
+    editTask(index, description) {
+        this.listArray[index - 1].edit = false;
+        this.updateTasks();
+    }
+    changeComplete(i) {
+        const status = this.listArray[i - 1].completed;
+        this.listArray[i -1] = {
+            ...this.listArray[i - 1],
+            completed: !status,
+        };
+        this.updateTasks();
+    }
 }
