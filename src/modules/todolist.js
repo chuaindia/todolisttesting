ccb661eb246deaba7dfc0f7c4dfd0ae06ce20037
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-10-15 00:09:20 +0530

    Add updated js file for linterfree

--- src/modules/todolist.js
+++ src/modules/todolist.js
@@ -2,13 +2,17 @@ export default class ToDoList {
   constructor() {
     this.getTasks();
   }
+  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
   updateTasks() {
     localStorage.setItem('toDoList', JSON.stringify(this.listArray));
   }
+  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
   getTasks() {
     this.listArray = JSON.parse(localStorage.getItem('toDoList')) || [];
   }
+  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
   showTasks = () => JSON.parse(localStorage.getItem('toDoList')) || [];
+  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
   setEdit(i) {
     const task = this.listArray.find(
       (item) => parseInt(item.index, 10) === parseInt(i, 10),
@@ -16,6 +20,7 @@ export default class ToDoList {
     task.edit = true;
     this.updateTasks();
   }
+  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
   addTask(description) {
     const task = {
       description,
@@ -26,6 +31,7 @@ export default class ToDoList {
     this.listArray = [...this.listArray, task];
     this.updateTasks();
   }
+  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
   clearCompleted() {
     this.listArray = this.listArray.filter((item) => item.completed !== true);
     if (this.listArray.length > 0) {
@@ -36,6 +42,7 @@ export default class ToDoList {
     }
     this.updateTasks();
   }
+  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
   removeTask(index) {
     this.listArray = this.listArray.filter((item) => item.index !== index);
     this.listArray = this.listArray.map((list, i) => {
@@ -44,11 +51,13 @@ export default class ToDoList {
     });
     this.updateTasks();
   }
+  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
   editTask(index, description) {
     this.listArray[index - 1].description = description;
     this.listArray[index - 1].edit = false;
     this.updateTasks();
   }
+  /* eslint lines-between-class-members: ["error", "always", { "exceptAfterSingleLine": true }]*/
   changeComplete(i) {
     const status = this.listArray[i - 1].completed;
     this.listArray[i - 1] = {
